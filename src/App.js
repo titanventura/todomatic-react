36df9d91c15fe948bd570d3b4c16bcdff4654dae
Aswath S <aswath.18cs@kct.ac.in>
2022-03-17 23:12:38 +0530

    Filter tests pending

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TodoInput from "./components/TodoInput/TodoInput";
 import { useState } from "react";
 import TodoList from "./components/TodoList/TodoList";
 import constants from "./constants";
+import Filter from "./components/Filter/Filter";
 
 function App(props) {
 	const { tasks: initialTasks } = constants;
@@ -11,41 +12,47 @@ function App(props) {
 	const addTask = (newTask) => {
 		setTasks([...tasks, newTask]);
 	};
+
+	const deleteTask = (taskId) => {
+		setTasks(tasks.filter((task) => task.id !== taskId));
+	};
+
+	const editTask = (id, newName) => {
+		setTasks(
+			tasks.map((task) =>
+				task.id === id ? { ...task, name: newName } : task
+			)
+		);
+	};
+
+	const toggleComplete = (id) => {
+		setTasks(
+			tasks.map((task) =>
+				task.id === id
+					? { ...task, isComplete: !task.isComplete }
+					: task
+			)
+		);
+	};
+
 	return (
 		<div className="todoapp stack-large">
 			<Heading />
 			<TodoInput addTask={addTask} />
 			<div className="filters btn-group stack-exception">
-				<button
-					type="button"
-					className="btn toggle-btn"
-					aria-pressed="true"
-				>
-					<span className="visually-hidden">Show </span>
-					<span>all</span>
-					<span className="visually-hidden"> tasks</span>
-				</button>
-				<button
-					type="button"
-					className="btn toggle-btn"
-					aria-pressed="false"
-				>
-					<span className="visually-hidden">Show </span>
-					<span>Active</span>
-					<span className="visually-hidden"> tasks</span>
-				</button>
-				<button
-					type="button"
-					className="btn toggle-btn"
-					aria-pressed="false"
-				>
-					<span className="visually-hidden">Show </span>
-					<span>Completed</span>
-					<span className="visually-hidden"> tasks</span>
-				</button>
+			{[1, 2, 3].map((e) => (
+				<Filter key={e} name="sm" isPressed={false} />
+			))}
 			</div>
-			<h2 id="list-heading">3 tasks remaining</h2>
-			<TodoList tasks={tasks} />
+			<h2 id="list-heading" data-testid="reamaining-task-text">
+				{tasks.length} {tasks.length === 1 ? "task" : "tasks"} remaining
+			</h2>
+			<TodoList
+				tasks={tasks}
+				deleteTask={deleteTask}
+				editTask={editTask}
+				toggleComplete={toggleComplete}
+			/>
 		</div>
 	);
 }
